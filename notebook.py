5b9b9f9daaf2de30deb7f15e899db62e74699c54
wangshoujunnew <37709822+wangshoujunnew@users.noreply.github.com>
2018-11-29 10:31:28 +0800

    Update notebook.py

--- notebook.py
+++ notebook.py
@@ -11,3 +11,10 @@
 # 构件图的形式
 创建常量     # matrix1 = tf.constant([[3., 3.]])    op1
 加减乘除操作 # product = tf.matmul(matrix1, matrix2) op2
+
+# GPU如何参与运算->指明GPU
+with tf.Session() as sess:
+  with tf.device("/gpu:1"): # /cpu:0 机器的CPU, /gpu:0 机器的第一个GPU,如果有的话
+    matrix1 = ...
+    matrix2 = ...
+    product = tf.matmul(matrix1, matrix2)
