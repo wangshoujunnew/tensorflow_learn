e38ce9cc66075ba8e144bae5cf185d1ae29a45c0
wangshoujunnew <37709822+wangshoujunnew@users.noreply.github.com>
2018-12-18 17:42:21 +0800

    Update producer.py

--- kafka/producer.py
+++ kafka/producer.py
@@ -1,7 +1,7 @@
 #encoding=utf-8
 from kafka import KafkaClient, KafkaProducer, KafkaConsumer
 
-# 给kafka集群发送数据
+# 给kafka集群发送数据,生产者
 cluster_host = '172.31.11.221:9092,172.31.11.222:9092,172.31.11.223:9092'
 topic = 'test_topic'
 group_id = 'test-python-test_topic'
