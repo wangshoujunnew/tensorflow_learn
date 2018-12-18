9eea272028e69eddeabdab1a243d34dffbb06010
wangshoujunnew <37709822+wangshoujunnew@users.noreply.github.com>
2018-12-18 17:41:44 +0800

    Create producer.py

---
+++ kafka/producer.py
@@ -0,0 +1,10 @@
+#encoding=utf-8
+from kafka import KafkaClient, KafkaProducer, KafkaConsumer
+
+# 给kafka集群发送数据
+cluster_host = '172.31.11.221:9092,172.31.11.222:9092,172.31.11.223:9092'
+topic = 'test_topic'
+group_id = 'test-python-test_topic'
+producter = KafkaProducer(bootstrap_servers=cluster_host)
+producter.send(topic=topic, value='from shoujunw test ... ')
+producter.flush()
